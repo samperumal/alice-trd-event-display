ca2458a52afcd3f40c3781a2c17dac23f1b593ab
Sam Perumal <sam_lki@yahoo.com>
2019-05-22 00:28:32 +0200

    Draw path, highlight selected

--- components/supermodule.js
+++ components/supermodule.js
@@ -11,9 +11,13 @@ class SuperModuleComponent extends ComponentBase {
         xscale.domain([-450, 450]);
         yscale.domain([-450, 450]);
 
+        this.line = d3.line().x(d => xscale(d.x)).y(d => yscale(d.y));
+
         this.container.classed("supermodule-component", true);
 
         this.detectors = this.container
+            .append("g")
+            .attr("class", "detectors")
             .selectAll("g.detector")
             .data(this.detectorData)
             .enter()
@@ -28,10 +32,34 @@ class SuperModuleComponent extends ComponentBase {
             .attr("height", d => dist(d.minLocalY, d.maxLocalY, yscale))
             .attr("width", d => dist(d.minR / 10, d.maxR / 10, xscale));
 
+        this.tracks = this.container.append("g")
+            .attr("class", "tracks");
+
         super.draw();
     }
 
     draw(eventData) {
+        const xscale = this.xscale, yscale = this.yscale;
+        const line = this.line;
 
+        const selectedTrack = eventData.trdTrack != null ? eventData.trdTrack.id : null;
+
+        let tracks = this.tracks
+            .selectAll("g.track")
+            .data(eventData.event.trdTracks.filter(d => d.track != null && d.track.path != null), d => d.id);
+
+        tracks.exit().remove();
+
+        tracks.enter()
+            .append("g")
+            .attr("class", "track")
+            .append("path")
+            .attr("class", "track")
+            .attr("d", d => line(d.track.path));
+
+        tracks = tracks.merge(tracks.enter());
+
+        tracks.selectAll("path.track")
+            .classed("selected", d => d.id == selectedTrack);            
     }
 }
