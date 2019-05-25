8dd3ab426f902863113c4b698c363fb0b9f3317c
Sam Perumal <sam_lki@yahoo.com>
2019-05-26 00:33:00 +0200

    Add track information component

---
+++ components/track-information.js
@@ -0,0 +1,31 @@
+class TrackInformationComponent {
+    constructor(id, config) {
+        const div = this.div = d3.select(id);
+
+        this.elements = {};
+
+        this.addElements({
+            "ID: ": "id",
+            "pT: ": "pt",
+            "PID: ": "pid",
+            "Stack": "stack",
+            "Sector": "sector"
+        });
+    }
+
+    addElements(elements) {
+        for (const el in elements) {
+            const tr = this.div.append("tr");
+            tr.append("td").text(el);
+            this.elements[elements[el]] = tr.append("td");
+        }
+    }
+
+    draw(eventData) {
+        if (eventData.trdTrack != null && eventData.trdTrack.trdTracklets != null) {
+            for (const el in this.elements) {
+                this.elements[el].text(eventData.trdTrack[el]);
+            }
+        }
+    }
+}
