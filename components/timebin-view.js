554f90b654bc5987554e49ee6e458036b444687e
Sam Perumal <sam_lki@yahoo.com>
2019-06-21 19:52:42 +0200

    Display histogram of counts per time bin

--- components/timebin-view.js
+++ components/timebin-view.js
@@ -73,7 +73,7 @@ class TbsumSubView {
 
         this.dimensions = getDimensions();
 
-        this.yscale = d3.scaleBand().domain(d3.range(30)).range([20, height - 20]).paddingInner(0.1);
+        this.yscale = d3.scaleBand().domain(d3.range(30)).range([20, height - 20]).paddingInner(0.2).paddingOuter(0.2);
 
         this.xscale = d3.scaleLinear().domain([1000, 0]).range([20, width - 20]);
 
@@ -85,8 +85,8 @@ class TbsumSubView {
             .attr("transform", `translate(0, ${height - 20})`)
             .call(d3.axisBottom(this.xscale));
 
-        this.content = this.tbsumContainer.append("g")
-            .attr("transform", `translate(${this.xscale.range()[0]}, ${this.yscale.range()[0]})`);
+        this.content = this.tbsumContainer.append("g");
+            //.attr("transform", `translate(${this.xscale.range()[0]}, ${this.yscale.range()[0]})`);
     }
 
     draw(trdTrackletData, digitsData) {
@@ -99,11 +99,37 @@ class TbsumSubView {
 
         const pads = digitsData.pads.filter(p => p.row == trdTrackletData.binZ && padIndices.includes(p.col));
 
-        const maxAdcCount = Math.max(d3.max(pads, p => p.tsum), 1000);
+        const tbinSum = d3.range(30);
+        for (const index in tbinSum)
+            tbinSum[index] = 0;
+
+        for (const pad of pads) {
+            for (let i = 0; i < 30; i++) 
+                tbinSum[i] += pad.tbins[i];
+        }
+
+        const maxAdcCount = Math.max(d3.max(tbinSum) * 1.1, 100);
 
         this.xscale.domain([maxAdcCount, 0]);
 
         this.xaxis.call(d3.axisBottom(this.xscale));
+
+        console.log(tbinSum);
+
+        const allRects = this.content.selectAll("rect.tbsum")
+            .data(tbinSum);
+
+        allRects.exit().remove();
+
+        const xscale = this.xscale, yscale = this.yscale;
+
+        allRects.enter().append("rect").attr("class", "tbsum")
+            .attr("y", (d, i) => yscale(i))
+            .attr("height", yscale.bandwidth());
+
+        this.content.selectAll("rect.tbsum")
+            .attr("x", d => xscale(d))
+            .attr("width", d => xscale(0) -xscale(d));
     }
 }
 
@@ -116,7 +142,7 @@ class PadSubView {
 
         this.dimensions = getDimensions();
 
-        this.yscale = d3.scaleBand().domain(d3.range(30)).range([20, height - 20]).paddingInner(0.1);
+        this.yscale = d3.scaleBand().domain(d3.range(30)).range([20, height - 20]).paddingInner(0.2).paddingOuter(0.2);
 
         this.xscale = d3.scaleBand().domain(d3.range(90, 99)).range([0, width - 20]);
 
