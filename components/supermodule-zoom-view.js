9dc73c6e50ba3c771644285dcbeb4b08b9947516
Sam Perumal <sam_lki@yahoo.com>
2019-07-22 12:44:48 +0200

    Use lookup table for stack geometry

--- components/supermodule-zoom-view.js
+++ components/supermodule-zoom-view.js
@@ -4,6 +4,9 @@ class SupermoduleZoomViewComponent extends ComponentBase {
 
         const stackDimensionData = this.stackDimensionData = getStackDimensions();
 
+        const padData = geomStackZRPlanePads().filter(d => d.d != null && d.d.length > 0);
+        const moduleData = geomStackZRPlaneModules();
+
         this.config = config != null ? config : {};
         this.r = (this.config.r != null) ? this.config.r : 2;
 
@@ -12,6 +15,32 @@ class SupermoduleZoomViewComponent extends ComponentBase {
         this.line = d3.line().x(d => xscale(-d.z)).y(d => yscale(d.r));
         this.line2 = d3.line().x(d => xscale(d.x)).y(d => yscale(d.y));
 
+        // Create map of paths per stack, for efficiency
+        this.stackPaths = new Map();
+
+        for (const stack in d3.range(5)) {
+            const stackbb = this.stackDimensionData[stack];
+
+            this.xscale.domain(stackbb.bbZ);
+            this.yscale.domain(stackbb.bbR);
+
+            const modulePath = moduleData
+                .filter(d => d.stk == stack)
+                .map(d => this.line2(d.d) + " Z ")
+                .join(" ");
+
+            const padPath = padData
+                .filter(d => d.s == stack && d.d != null && d.d.length > 0)
+                .map(d => this.line2(d.d) + " Z ")
+                .join(" ");
+
+            this.stackPaths.set(+stack, {
+                modulePath: modulePath,
+                padPath: padPath
+            });
+        }
+
+        // Create elements
         this.container
             .attr("class", "supermodule-view-component");
 
@@ -54,28 +83,18 @@ class SupermoduleZoomViewComponent extends ComponentBase {
         const line = this.line;
 
         if (eventData.track != null) {
-            const stack = this.stackDimensionData[eventData.track.stk];
+            const stackbb = this.stackDimensionData[eventData.track.stk];
 
-            this.xscale.domain(stack.bbZ);
-            this.yscale.domain(stack.bbR);
+            this.xscale.domain(stackbb.bbZ);
+            this.yscale.domain(stackbb.bbR);
 
-            const padRowDimensionData = geomStackZRPlanePads();
+            this.pads.attr("d", this.stackPaths.get(eventData.track.stk).padPath);
 
-            const detectorPath = padRowDimensionData
-                .filter(d => d.s == eventData.track.stk && d.d != null && d.d.length > 0)
-                .map(d => this.line2(d.d) + " Z ")
-                .join(" ");
-
-            this.pads.attr("d", detectorPath);
-
-            const modulePath = geomStackZRPlaneModules()
-                .filter(d => d.stk == eventData.track.stk)
-                .map(d => this.line2(d.d) + " Z ")
-                .join(" ");
-
-            this.modules.attr("d", modulePath);
+            this.modules.attr("d", this.stackPaths.get(eventData.track.stk).modulePath);
 
             this.selectedTrack.attr("d", line(eventData.track.path));
+
+            const padRowDimensionData = geomStackZRPlanePads();
 
             this.selectedTracklets.attr("d", eventData.track.trklts
                 .map(d => padRowDimensionData[rid(d.stk, d.lyr, d.row)])
@@ -86,7 +105,7 @@ class SupermoduleZoomViewComponent extends ComponentBase {
         else {
             this.selectedTrack.attr("d", null);
             this.selectedTracklets.attr("d", null);
-            this.detectors.attr("d", null);
+            this.pads.attr("d", null);
             this.modules.attr("d", null);
         }
     }
