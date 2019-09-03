d0d983334d109b236feda2b1298b777e0cf8ffed
Sam Perumal <sam_lki@yahoo.com>
2019-09-03 20:05:25 +0200

    Add trigger information text

--- components/track-information.js
+++ components/track-information.js
@@ -3,6 +3,8 @@ class TrackInformationComponent {
         const div = this.div = d3.select(id);
         this.info_event = div.select("div.info-event");
         this.info_track = div.select("div.info-track");
+        this.info_tracklets = div.select("div.info-tracklets");
+        this.info_triggers = div.select("div.info-triggers");
 
         this.elements = {};
 
@@ -23,6 +25,7 @@ class TrackInformationComponent {
 
     draw(eventData) {
         let event_text = "";
+        let trigger_text = "";
 
         if (eventData.event != null) {
             const ev = eventData.event;
@@ -33,20 +36,36 @@ class TrackInformationComponent {
                     const energy = (ev.i.be / 1000) + " TeV";
                     event_text = `Collision between ${collision_system} at an energy of ${energy}`;
                 }
-            }            
+
+                if (ev.i.ft != null) {
+                    const triggers = ev.i.ft.trim().split("  ");
+                    trigger_text = `${triggers.length} high-level triggers fired for this event: ${triggers.join(", ")}`;
+                }
+            }
         }
         this.info_event.html(event_text);
+        this.info_triggers.html(trigger_text);
 
         let track_text = "";
         if (eventData.track != null) {
             const track = eventData.track;
             const info = track.i;
-            
+
             track_text = `${track.typ} track ${track.id} traverses Sector ${track.sec}, Stack ${track.stk} of the TRD`;
             if (info.pT != null) track_text += ` with a transverse momentum of ${info.pT} eV.`; else track_text += ".";
 
             if (info.pid != null) track_text += ` The calculated PID value of ${info.pid} indicates this is likely ${info.pid >= 100 ? "an electron" : "a pion"} track.`;
         }
         this.info_track.html(track_text);
+
+        let tracklet_text = "None";
+        if (eventData.track.trklts != null && eventData.track.trklts.length > 0) {
+            tracklet_text = eventData.track.trklts.length;
+        }
+        else {
+
+        }
+
+        this.info_tracklets.html(`${tracklet_text} of the ${eventData.event.trklts.length} tracklets detected by the TRD have been matched to this track.`);
     }
 }
